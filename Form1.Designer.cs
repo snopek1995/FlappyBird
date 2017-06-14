3154307dfd2272baf99375fdbbe9167b1255d7e9
tysiacemysli <tysiacemysli@wp.pl>
2017-06-14 23:44:49 +0200

    Button

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -37,17 +37,22 @@
             this.Dolna_rura = new System.Windows.Forms.PictureBox();
             this.Postac = new System.Windows.Forms.PictureBox();
             this.Ziemia = new System.Windows.Forms.PictureBox();
+            this.Start_b = new System.Windows.Forms.Button();
+            this.Dolna_rura2 = new System.Windows.Forms.PictureBox();
+            this.Gorna_rura2 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.Gorna_rura)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.Dolna_rura)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.Postac)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.Ziemia)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Dolna_rura2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Gorna_rura2)).BeginInit();
             this.SuspendLayout();
             // 
             // Wynik_text
             // 
             this.Wynik_text.AutoSize = true;
             this.Wynik_text.BackColor = System.Drawing.Color.Transparent;
-            this.Wynik_text.Location = new System.Drawing.Point(132, 9);
+            this.Wynik_text.Location = new System.Drawing.Point(165, 9);
             this.Wynik_text.Name = "Wynik_text";
             this.Wynik_text.Size = new System.Drawing.Size(34, 13);
             this.Wynik_text.TabIndex = 4;
@@ -57,7 +62,7 @@
             // 
             this.Koniec_text1.AutoSize = true;
             this.Koniec_text1.BackColor = System.Drawing.Color.Transparent;
-            this.Koniec_text1.Location = new System.Drawing.Point(115, 143);
+            this.Koniec_text1.Location = new System.Drawing.Point(148, 143);
             this.Koniec_text1.Name = "Koniec_text1";
             this.Koniec_text1.Size = new System.Drawing.Size(68, 13);
             this.Koniec_text1.TabIndex = 5;
@@ -67,7 +72,7 @@
             // 
             this.Koniec_text2.AutoSize = true;
             this.Koniec_text2.BackColor = System.Drawing.Color.Transparent;
-            this.Koniec_text2.Location = new System.Drawing.Point(106, 182);
+            this.Koniec_text2.Location = new System.Drawing.Point(139, 182);
             this.Koniec_text2.Name = "Koniec_text2";
             this.Koniec_text2.Size = new System.Drawing.Size(87, 13);
             this.Koniec_text2.TabIndex = 6;
@@ -83,7 +88,7 @@
             // 
             this.Gorna_rura.BackColor = System.Drawing.Color.Transparent;
             this.Gorna_rura.Image = global::FlappyBird.Properties.Resources.pipedown;
-            this.Gorna_rura.Location = new System.Drawing.Point(187, -4);
+            this.Gorna_rura.Location = new System.Drawing.Point(222, -1);
             this.Gorna_rura.Name = "Gorna_rura";
             this.Gorna_rura.Size = new System.Drawing.Size(91, 171);
             this.Gorna_rura.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -94,7 +99,7 @@
             // 
             this.Dolna_rura.BackColor = System.Drawing.Color.Transparent;
             this.Dolna_rura.Image = global::FlappyBird.Properties.Resources.pipe;
-            this.Dolna_rura.Location = new System.Drawing.Point(114, 285);
+            this.Dolna_rura.Location = new System.Drawing.Point(168, 319);
             this.Dolna_rura.Name = "Dolna_rura";
             this.Dolna_rura.Size = new System.Drawing.Size(91, 126);
             this.Dolna_rura.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -106,9 +111,9 @@
             // 
             this.Postac.BackColor = System.Drawing.Color.Transparent;
             this.Postac.Image = global::FlappyBird.Properties.Resources.bird_straight;
-            this.Postac.Location = new System.Drawing.Point(12, 80);
+            this.Postac.Location = new System.Drawing.Point(12, 88);
             this.Postac.Name = "Postac";
-            this.Postac.Size = new System.Drawing.Size(49, 51);
+            this.Postac.Size = new System.Drawing.Size(47, 43);
             this.Postac.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.Postac.TabIndex = 1;
             this.Postac.TabStop = false;
@@ -117,12 +122,45 @@
             // 
             this.Ziemia.BackColor = System.Drawing.Color.Transparent;
             this.Ziemia.Image = global::FlappyBird.Properties.Resources.ground;
-            this.Ziemia.Location = new System.Drawing.Point(-7, 370);
+            this.Ziemia.Location = new System.Drawing.Point(-11, 404);
             this.Ziemia.Name = "Ziemia";
             this.Ziemia.Size = new System.Drawing.Size(570, 41);
             this.Ziemia.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.Ziemia.TabIndex = 7;
             this.Ziemia.TabStop = false;
+            // 
+            // Start_b
+            // 
+            this.Start_b.BackColor = System.Drawing.Color.MintCream;
+            this.Start_b.Location = new System.Drawing.Point(134, 248);
+            this.Start_b.Name = "Start_b";
+            this.Start_b.Size = new System.Drawing.Size(96, 31);
+            this.Start_b.TabIndex = 8;
+            this.Start_b.Text = "Start";
+            this.Start_b.UseVisualStyleBackColor = false;
+            this.Start_b.Click += new System.EventHandler(this.Start_b_Click);
+            // 
+            // Dolna_rura2
+            // 
+            this.Dolna_rura2.BackColor = System.Drawing.Color.Transparent;
+            this.Dolna_rura2.Image = global::FlappyBird.Properties.Resources.pipe;
+            this.Dolna_rura2.Location = new System.Drawing.Point(468, 267);
+            this.Dolna_rura2.Name = "Dolna_rura2";
+            this.Dolna_rura2.Size = new System.Drawing.Size(91, 251);
+            this.Dolna_rura2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Dolna_rura2.TabIndex = 9;
+            this.Dolna_rura2.TabStop = false;
+            // 
+            // Gorna_rura2
+            // 
+            this.Gorna_rura2.BackColor = System.Drawing.Color.Transparent;
+            this.Gorna_rura2.Image = global::FlappyBird.Properties.Resources.pipedown;
+            this.Gorna_rura2.Location = new System.Drawing.Point(561, -1);
+            this.Gorna_rura2.Name = "Gorna_rura2";
+            this.Gorna_rura2.Size = new System.Drawing.Size(91, 132);
+            this.Gorna_rura2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Gorna_rura2.TabIndex = 10;
+            this.Gorna_rura2.TabStop = false;
             // 
             // Form1
             // 
@@ -130,14 +168,17 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.Color.MediumTurquoise;
-            this.ClientSize = new System.Drawing.Size(365, 407);
-            this.Controls.Add(this.Ziemia);
-            this.Controls.Add(this.Koniec_text2);
-            this.Controls.Add(this.Koniec_text1);
+            this.ClientSize = new System.Drawing.Size(365, 439);
             this.Controls.Add(this.Wynik_text);
+            this.Controls.Add(this.Koniec_text1);
+            this.Controls.Add(this.Koniec_text2);
+            this.Controls.Add(this.Gorna_rura2);
+            this.Controls.Add(this.Start_b);
+            this.Controls.Add(this.Ziemia);
             this.Controls.Add(this.Gorna_rura);
             this.Controls.Add(this.Dolna_rura);
             this.Controls.Add(this.Postac);
+            this.Controls.Add(this.Dolna_rura2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
             this.Name = "Form1";
             this.Text = "Wynik";
@@ -148,6 +189,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.Dolna_rura)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.Postac)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.Ziemia)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Dolna_rura2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Gorna_rura2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -163,6 +206,9 @@
         private System.Windows.Forms.Label Koniec_text2;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.PictureBox Ziemia;
+        private System.Windows.Forms.Button Start_b;
+        private System.Windows.Forms.PictureBox Dolna_rura2;
+        private System.Windows.Forms.PictureBox Gorna_rura2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,17 +7,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace FlappyBird
 {
     public partial class Form1 : Form
     {
-       
+        bool skok = false;
         int grawitacja = 3;
         int predkosc = 3;
         int wynik = 0;
-       // int koniec = 1;
-        //int sufit = Screen.PrimaryScreen.Bounds.Heig;
 
         Random los = new Random();
 
@@ -31,6 +30,7 @@ namespace FlappyBird
             Koniec_text1.Visible = false; //niewidoczny
             Koniec_text2.Visible = false;
             Wynik_text.Text = "" + wynik;
+            Start_b.Visible = false;
 
         }
 
@@ -43,12 +43,15 @@ namespace FlappyBird
 
 
         }
-
+        
         private void timer1_Tick(object sender, EventArgs e)
         {
             Dolna_rura.Left -= predkosc; // brak przesuniecia rury w prawo
+            Dolna_rura2.Left -= predkosc;
             Gorna_rura.Left -= predkosc;
+            Gorna_rura2.Left -= predkosc;
             Postac.Top += grawitacja;   // ruch postaci
+
 
             // zderzenia 
             if(Postac.Bounds.IntersectsWith(Ziemia.Bounds))
@@ -61,30 +64,60 @@ namespace FlappyBird
                 koniec_gry();
             }
 
+            else if (Postac.Bounds.IntersectsWith(Gorna_rura2.Bounds))
+            {
+                koniec_gry();
+            }
+
             else if(Postac.Bounds.IntersectsWith(Dolna_rura.Bounds))
+            {
+                koniec_gry();
+            }
+            else if (Postac.Bounds.IntersectsWith(Dolna_rura2.Bounds))
             {
                 koniec_gry();
             }
 
 
             // pojawianie się rur 
-            if ( Dolna_rura.Right < -93) 
+            if ( Dolna_rura.Right < 0 ) 
             {
-                Dolna_rura.Left = los.Next(323, 553);
+                Dolna_rura.Left = los.Next(381, 482);
                 wynik += 1 ;
                 Wynik_text.Text = " " + wynik;
-                Dolna_rura.Size = new Size(91, los.Next(81, 131));
+                Dolna_rura.Size = new Size(91, los.Next(81, 216));
+
             }
 
-
-            if (Gorna_rura.Left < -93)
+            if (Dolna_rura2.Right < 0)
             {
-                Gorna_rura.Left = los.Next(352, 535);
+                Dolna_rura2.Left = los.Next(381, 482)+30;
                 wynik += 1;
                 Wynik_text.Text = " " + wynik;
-                Gorna_rura.Size = new Size(91, los.Next(80, 131));
+                Dolna_rura2.Size = new Size(91, los.Next(81, 216));
             }
+
+            if (Gorna_rura.Right  < 0)
+            {
+                Gorna_rura.Left = los.Next(381, 535);
+                wynik += 1;
+                Wynik_text.Text = " " + wynik;
+                Gorna_rura.Size = new Size(91, los.Next(80, 216));
+            }
+
+
+            if (Gorna_rura2.Right < 0)
+            {
+                Gorna_rura2.Left = los.Next(381, 529);
+                wynik += 1;
+                Wynik_text.Text = " " + wynik;
+                Gorna_rura2.Size = new Size(91, los.Next(80, 216));
+            }
+
+
+
         }
+
 
         // Zmiana lotu birdsa [ spacja - zmiana z lotu na dol na lot do gory
         private void Form1_KeyDown(object sender, KeyEventArgs e)
@@ -92,6 +125,8 @@ namespace FlappyBird
             if (e.KeyCode == Keys.Space)
             {
                 grawitacja = -3;
+                skok = true;
+                
             }
 
         }
@@ -102,6 +137,7 @@ namespace FlappyBird
             if(e.KeyCode == Keys.Space)
             {
                 grawitacja = 2;
+                skok = true;
             }
         }
 
@@ -116,6 +152,7 @@ namespace FlappyBird
             Koniec_text1.Visible = true;
             Koniec_text2.Visible = true;
             Wynik_text.Text = " " + wynik;
+            Start_b.Visible = true;
 
         }
 
@@ -129,6 +166,12 @@ namespace FlappyBird
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void Start_b_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+                
         }
     }
 }
